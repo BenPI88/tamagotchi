bef89eed4e9b043c0f62cb688bba6d5c986cdc78
BenPI88 <97198967+BenPI88@users.noreply.github.com>
2022-08-14 12:00:02 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+import os
+import time
+os.system("clear")
+print("Tamagotchi Remade By BenPI88 (https://github.com/BenPI88)")
+input("")
+def clearscreen:
+  os.system("clear")
+clearscreen
+print("Setting Up Folders")
+dir = "~/BenPI/tamagotchi/"
+os.system("cd ~ && mkdir BenPI && cd BenPI && mkdir tamagotchi")
+print("Done.")
+time.sleep(1)
